c2cd255c08af78907e4d9ec657547d1f47db45cc
Joshua <joshualicht19@gmail.com>
2023-01-02 17:54:06 -0600

    added solution for e17

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,3 +1,25 @@
-export function minBy(array, cb) {}
+export function minBy(array, cb) {
+  let minValue;
+  let minElement;
+  for (let i = 0; i < array.length; i++) {
+    const value = cb(array[i]);
+    if (minValue === undefined || value < minValue) {
+      minValue = value;
+      minElement = array[i];
+    }
+  }
+  return minElement;
+}
 
-export function maxBy(array, cb) {}
+export function maxBy(array, cb) {
+  let maxValue;
+  let maxElement;
+  for (let i = 0; i < array.length; i++) {
+    const value = cb(array[i]);
+    if (maxValue === undefined || value > maxValue) {
+      maxValue = value;
+      maxElement = array[i];
+    }
+  }
+  return maxElement;
+}
